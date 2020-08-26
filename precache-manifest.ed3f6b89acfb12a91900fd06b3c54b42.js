312d012c53237d908bcfe130fcef8342cae7f7ef
Matt Seidholz <mseidholz@gmail.com>
2020-08-26 15:54:33 -0400

    Updates

---
+++ precache-manifest.ed3f6b89acfb12a91900fd06b3c54b42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c87f9730c036fd31a8b6c6b2283638",
+    "url": "/ava-frontend/index.html"
+  },
+  {
+    "revision": "32131bc55b490f570848",
+    "url": "/ava-frontend/static/css/main.c6b91a21.chunk.css"
+  },
+  {
+    "revision": "731b42e3bd6373bfaccf",
+    "url": "/ava-frontend/static/js/2.349b55e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ava-frontend/static/js/2.349b55e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32131bc55b490f570848",
+    "url": "/ava-frontend/static/js/main.8a1d1df3.chunk.js"
+  },
+  {
+    "revision": "de22582e03beb433850e",
+    "url": "/ava-frontend/static/js/runtime-main.63244ce7.js"
+  }
+]);
